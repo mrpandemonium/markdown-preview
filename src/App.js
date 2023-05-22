de5cb5d07566688c79179d55a02db693fea03b0e
mrpandemonium <wincentsouriya@gmail.com>
2023-05-22 08:38:21 -0700

    finish

--- src/App.js
+++ src/App.js
@@ -3,7 +3,29 @@ import { useState } from "react";
 import { marked } from "marked";
 
 function App() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+  [title](https://www.example.com)
+  \`code\`
+  \`\`\`
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+  \`\`\`
+  - First item
+  - Second item
+  - Third item
+
+  >blockquote
+
+  ![alt text](image.jpg)
+  
+  **bold text**
+
+  `);
 
   return (
     <div className="App">
